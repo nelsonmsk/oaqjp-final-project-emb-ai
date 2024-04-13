72f711902dab33064d0bf59d3cb3d6d1af738a47
Nelsonmsk <nelsonmsk@gmail.com>
2024-04-13 13:51:25 -0400

    Fix emotion detion code

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,11 +8,11 @@ def emotion_detector(text_to_analyse):
     response = requests.post(url, json = myobj, headers=header)
     formatted_response = json.loads(response.text)
     dict_response = formatted_response['emotionPredictions'][0]
-    anger = dict_response['emotion']['anger']
-    disgust = dict_response['emotion']['disgust']
-    fear = dict_response['emotion']['fear']
-    joy = dict_response['emotion']['joy']
-    sadness = dict_response['emotion']['sadness']
     emotion = dict_response['emotion']
+    anger = emotion['anger']
+    disgust = emotion['disgust']
+    fear = emotion['fear']
+    joy = emotion['joy']
+    sadness = emotion['sadness']
     max_key = [k for k in emotion.keys() if emotion[k] == max(emotion.values())]
     return {'anger': anger, 'disgust': disgust, 'fear': fear, 'joy': joy, 'sadness': sadness, 'dominant_emotion': max_key[0]}
